7bbad5608e5546006c1a9508e64e99e806c9d5c8
Muhammad Adrees <159925060+M-Adrees-gujjar@users.noreply.github.com>
2024-05-23 12:51:51 +0000

    mongoDB

--- src/models/connection.model.js
+++ src/models/connection.model.js
@@ -3,13 +3,12 @@ const mongoose = require('mongoose');
 const db_conection = async () => {
     try {
         // const ConnectionInstance = await mongoose.connect("mongodb://localhost:27017");
-        const ConnectionInstance = await mongoose.connect(`mongodb+srv://${process.env.MONGODB_NAME}:${process.env.MONGODB_PASSWORD}@cluster0.fyd5j7f.mongodb.net`);
+        const ConnectionInstance = await mongoose.connect(`mongodb+srv://adrees:adrees123@cluster0.fyd5j7f.mongodb.net`);
         console.log("Connected DataBase : ", ConnectionInstance.connection.host);
     } catch (error) {
         console.log("MongoDB Connection Failed", error);
     }
 }
-
 
 
 module.exports = db_conection;
